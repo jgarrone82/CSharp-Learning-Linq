b25147605d783db4f7dc20ac21639a1be5cc5fb0
jgarrone <jorge.garrone@gmail.com>
2022-12-14 13:37:59 +0100

    Add book and LinqQueries class

---
+++ LinqQueries.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace C__Learning_Linq
+{
+    public class LinqQueries
+    {
+        public LinqQueries()
+        {
+            
+        }
+    }
+}
